4fb7fac13c9a8ec9a791dd0508c51c0c131323da
Yuichi Fujiki <yuichi@chaione.com>
2017-04-28 22:02:05 -0500

    Expose NUIParse_iOS as public scheme.

---
+++ NUIParse_iOS/NUIParse_iOS/NUIParse_iOS.h
@@ -0,0 +1,19 @@
+//
+//  NUIParse_iOS.h
+//  NUIParse_iOS
+//
+//  Created by Yuichi Fujiki on 4/28/17.
+//  Copyright © 2017 ChaiOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NUIParse_iOS.
+FOUNDATION_EXPORT double NUIParse_iOSVersionNumber;
+
+//! Project version string for NUIParse_iOS.
+FOUNDATION_EXPORT const unsigned char NUIParse_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NUIParse_iOS/PublicHeader.h>
+
+
